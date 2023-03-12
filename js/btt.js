98b475306fe79968719b376a89a260928016f9c9
Pierre Matterne <pierre.matterne@gmail.com>
2023-03-12 18:51:10 +0100

    UPDATE for mobile version

--- js/btt.js
+++ js/btt.js
@@ -8,13 +8,13 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
-}else{
+  }else{
     mybutton.style.display = "none";
-}
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function backToTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+  } 
--- js/slider.js
+++ js/slider.js
@@ -13,23 +13,23 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("custom-slider");
   console.log(slides);
-
   var dots = document.getElementsByClassName("dot");
   console.log(dots);
-
   if (n > slides.length) {slideIndex = 1} 
-
-  if (n < 1) {slideIndex = slides.length}
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex-1].style.display = "block";  
-
-  dots[slideIndex-1].className += " active";
-}
+    if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      /* ! Il y a une erreur à gérer ici */
+      try{
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+      }catch(e){
+        console.error("Une erreur est survenue :(");
+        console.log(e);
+      }
+/* ! Fin de l'erreur */
+    }
